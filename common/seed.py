34fe1cf50e03b6ea7671e47a6a16532ef55a0a0b
Bradley <bradleyhewitt0@gmail.com>
2026-06-11 22:14:04 +0100

    Added seed.py to ensure all results are reproducible and every result has a seed attached to it.

---
+++ common/seed.py
@@ -0,0 +1,16 @@
+"""Seeding utility to be called at the start of each run"""
+
+import random
+import numpy as np
+import torch
+
+def set_seed(seed: int, env=None) -> None:
+    """Seed Python, NumPy and torch"""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # Covers multiple GPUs if using multiple
+
+    # If Gymnasium environment is used, reset it with the same seed
+    if env is not None:
+        env.reset(seed=seed)
